c2feb7d5a480dea8bf52df51eb315504b8ba91cf
AmirArsalan <amirarsalan@AmirArsalans-Mac-Studio.local>
2025-11-27 13:20:54 +0330

    Auto commit

--- resources/js/components/AppNavigation.vue
+++ resources/js/components/AppNavigation.vue
@@ -12,7 +12,7 @@ const navItems = [
 
 <template>
     <nav class="fixed right-0 bottom-0 left-0 z-50 px-4 py-8">
-        <div class="mx-auto max-w-7xl rounded-4xl border-2 border-border px-4 backdrop-blur-xs">
+        <div class="mx-auto max-w-7xl overflow-hidden rounded-4xl border-2 border-border px-4 backdrop-blur-xs">
             <div class="flex items-center justify-around py-3">
                 <Link
                     v-for="item in navItems"

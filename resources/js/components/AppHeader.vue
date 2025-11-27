6550ef62c6b7cc9b017d3aed7b4b04a1bfbc887e
AmirArsalan <amirarsalan@AmirArsalans-Mac-Studio.local>
2025-11-27 13:11:43 +0330

    Auto commit

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -63,7 +63,7 @@ const rightNavItems: NavItem[] = [
         <div class="border-b border-sidebar-border/80">
             <div class="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
                 <!-- Mobile Menu -->
-                <div class="lg:hidden">
+                <div class="hidden">
                     <Sheet>
                         <SheetTrigger :as-child="true">
                             <Button variant="ghost" size="icon" class="h-9 w-9 ltr:mr-2 rtl:ml-2">
